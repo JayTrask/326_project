b3a43e05756f403f97d1466ad34f7edbeaa85c5d
omhiggins <omhiggins@umass.edu>
2018-11-27 07:22:41 +0000

    changed registration page

--- src/HiveListLibrary/HiveList/forms.py
+++ src/HiveListLibrary/HiveList/forms.py
@@ -8,11 +8,14 @@ from HiveList.models import Playlist
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(max_length=254)
 
     class Meta:
         model = User
         fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        help_texts = {
+                'username': ''
+                }
 
 
 
