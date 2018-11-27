b167e73c0c89d01324f0b85fbddbf1107788f5d0
Omar Sanchez <vagrant@vagrant.vm>
2018-11-27 07:20:58 +0000

    added user views, linked linked playlists to current playlist page, + other stuff I dont remember...

--- src/HiveListLibrary/init.py
+++ src/HiveListLibrary/init.py
@@ -67,7 +67,7 @@ for i in range(1, 200):
     p_creation_date = fake.date()
     p_description = fake.text(100)
     p_vote_time = fake.date_time()
-    p_ranking = fake.random_int(0, 100000)
+    p_ranking = i
     p_voting_threshold = fake.random_int(1, 101)
     p_id = fake.uuid4()
     playlist = Playlist(playlist_id=p_id, playlist_name=p_name, playlist_creation_date=p_creation_date,
