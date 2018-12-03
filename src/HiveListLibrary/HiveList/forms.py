743b2b48c43eb63f4bc8eafd029a87068a5e57fc
Collin Giguere <cdgiguere@umass.edu>
2018-12-03 23:42:17 +0000

    tried to get votes reflect the updates we give it and fixed some formatting

--- src/HiveListLibrary/HiveList/forms.py
+++ src/HiveListLibrary/HiveList/forms.py
@@ -4,7 +4,6 @@ from django.contrib.auth.models import User
 from django.forms.widgets import NumberInput
 from HiveList.models import Playlist, Song, SongInstance
 
-
 class SignUpForm(UserCreationForm):
     first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
     last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
@@ -18,31 +17,28 @@ class SignUpForm(UserCreationForm):
                 }
 
 
-
 class PlaylistCreationForm(forms.ModelForm):
+    playlist_name = forms.CharField(max_length=200, help_text="Enter a title for the playlist (e.g. Meat Bird Execution Playlist)")
+    playlist_description = forms.CharField(max_length=1000, help_text="Enter description for playlist")
+    playlist_vote_time = forms.DateTimeField()
+    playlist_votingthreshold = forms.IntegerField()
+    playlist_is_private = forms.BooleanField(required=False)
 
-	playlist_name = forms.CharField(max_length=200, help_text="Enter a title for the playlist (e.g. Meat Bird Execution Playlist)")
-	playlist_description = forms.CharField(max_length=1000, help_text="Enter description for playlist")
-	playlist_vote_time = forms.DateTimeField()
-	playlist_votingthreshold = forms.IntegerField()
-	playlist_is_private = forms.BooleanField(required=False)
-
-	class Meta:
-		model = Playlist
-		fields = [
-			'playlist_name',
-			'playlist_description',
-			'playlist_vote_time',
-			'playlist_votingthreshold',
-			'playlist_is_private'
-		]
-
-
+    class Meta:
+        model = Playlist
+        fields = [
+            'playlist_name',
+            'playlist_description',
+            'playlist_vote_time',
+            'playlist_votingthreshold',
+            'playlist_is_private'
+            ]
 
 
 class NamesChoiceField(forms.ModelChoiceField):
         def label_from_instance(self, obj):
                     return '{title} by {artist}'.format(title=obj.title, artist=obj.artist)
+
 
 class AddSongForm(forms.ModelForm):
     song_id = NamesChoiceField(label="Song", queryset=Song.objects.all())
@@ -53,9 +49,10 @@ class AddSongForm(forms.ModelForm):
 
 
 class VoteForm(forms.ModelForm):
+    number_yes_votes = forms.IntegerField()
+    number_no_votes = forms.IntegerField()
 
     class Meta:
         model = SongInstance
         fields = ['number_yes_votes','number_no_votes']
-        
 
